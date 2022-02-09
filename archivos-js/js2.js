400d21a57fbeb9d3015929c82230ffe7ed1eb9a7
Alvaro Sagredo <40775753+Al-Sagredo@users.noreply.github.com>
2022-02-09 20:15:44 -0300

    Delete archivos-js directory
    los cambié a la carpeta  uso de variables

--- archivos-js/js2.js
+++
@@ -1,7 +0,0 @@
-//se pide el input al usuario y se le asigna a la variable temp
-var temp = parseInt(prompt("Ingresa la temperatura en Celsius"));
-//se hacen las transformaciones y se asigna los resultados a las variables
-var kelvin = temp + 273.15
-var fahr = (temp *9/5) + 32;
-//se muestra al usuario los resultados
-alert(kelvin + " °K   " + fahr + " °F");
