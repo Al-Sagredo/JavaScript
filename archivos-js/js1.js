0209fee3b8251544af42e45b2e9a914954d117a3
TheIceCreaMan <alvergarax@gmail.com>
2022-02-09 20:07:47 -0300

    ejercicios terminados

---
+++ archivos-js/js1.js
@@ -0,0 +1,7 @@
+//se pide el input al usuario
+alert("Ingresa dos número mayores a 0");
+//se crean 2 variables con los valores ingresados
+var n1 = parseInt(prompt("Primer numero: "));
+var n2 = parseInt(prompt("Segundo numero: "));
+//se muestran al usuario los resultados de las operaciones
+alert("Suma: " +(n1+n2) + " Resta: " + (n1-n2) + " Multiplicacion: " +n1*n2 + " Division: " + (n1/n2) + " Modulo: " +  n1%n2);
